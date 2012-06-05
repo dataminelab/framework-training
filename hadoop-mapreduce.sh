e7ac850f45255b812359c44e5fd81c9fe1d39d79
rathko <rathko>
2012-06-05 17:56:45 -0400

    Fixed Cloudera paths

--- hadoop-mapreduce.sh
+++ hadoop-mapreduce.sh
@@ -1,1 +1,2 @@
-hadoop jar contrib/streaming/hadoop-*streaming*.jar -file ./mapper.py –mapper ./mapper.py –file ./reducer.py -reducer ./reducer.py -input /user/training/words.txt -output /user/training/words-output.tsv
+export HADOOP_HOME=/usr/lib/hadoop-0.20/contrib/
+hadoop jar $HADOOP_HOME/streaming/hadoop-streaming*.jar -file ./mapper.py -mapper ./mapper.py -file ./reducer.py -reducer ./reducer.py -input /user/training/words.txt -output /user/training/words-output
