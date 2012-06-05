0354b0f3c3f51ffd3c14d1837b42cbae5eceb7f9
DataMine Lab <radek@urbantrip.com>
2012-06-05 22:54:56 +0100

    Fixed Hadoop path

--- compile-wordcount.sh
+++ compile-wordcount.sh
@@ -1,3 +1,3 @@
-export HADOOP_HOME=/usr/lib/hadoop-0.20/contrib/
+export HADOOP_HOME=/usr/lib/hadoop-0.20
 javac -classpath ${HADOOP_HOME}/hadoop-*-core.jar -d java/target java/src/org/apache/WordCount.java
 jar -cvf ./wordcount.jar -C java/target .
