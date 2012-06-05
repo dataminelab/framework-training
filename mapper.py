a82f5743dc7642e45a54a210b43ff651db98b906
DataMine Lab <radek@urbantrip.com>
2012-06-05 18:22:30 +0100

    Removing punctuation

--- mapper.py
+++ mapper.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-import sys, string
+import sys, string, re
 
 for line in sys.stdin:
-    line = line.strip().translate(None, string.punctuation).lower()
+    line = line.strip().lower()
+    line = re.sub('[^A-Za-z0-9 ]+', '', line)
     words = line.split()
     for word in words:
         print '%s\t%s' % (word, 1)
