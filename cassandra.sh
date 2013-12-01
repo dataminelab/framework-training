bac1ba2a73e08c61b2deb1730093cb2bd4b534dd
rathko <radek@dataminelab.com>
2013-12-01 22:18:50 +0000

    Added sample install script

---
+++ cassandra.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+HOST="localhost"
+
+mkdir -p target
+cd target
+rm -fR ./cassandra
+
+# download cassandra
+version="apache-cassandra-2.0.0"
+file=${version}-bin.tar.gz
+wget -c http://archive.apache.org/dist/cassandra/2.0.0/${file}
+
+# extract
+tar xzf ./${file}
+mv ./${version} ./cassandra
+
+cd ..
+
+# change configs
+sed -e "s/listen_address: localhost/listen_address: $HOST/" \
+    -e "s/rpc_address: localhost/rpc_address: $HOST/" \
+    -e "s/seeds: "127.0.0.1"/seeds: "$HOST"/" \
+        < ./conf/cassandra.yaml > ./target/cassandra/conf/cassandra.yaml
+
+cp ./conf/cassandra-env.sh ./target/cassandra/conf/cassandra-env.sh
+cp ./conf/log4j-server.properties ./target/cassandra/conf/log4j-server.properties
+
+sed -e "s@CASSANDRA_PID_DIR@$CASSANDRA_PID_DIR@" \
+                ./start.sh > ./target/cassandra/bin/start.sh
+
+sed -e "s@CASSANDRA_PID_DIR@$CASSANDRA_PID_DIR@" \
+        < ./stop.sh > ./target/cassandra/bin/stop.sh
