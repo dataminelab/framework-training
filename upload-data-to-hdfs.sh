f1b1529af8339a8881c0f80e9e507e73b74eca1c
root <root@sandbox.hortonworks.com>
2014-09-30 05:50:35 -0700

    Bumped hdfs version

--- upload-data-to-hdfs.sh
+++ upload-data-to-hdfs.sh
@@ -1,2 +1,4 @@
-hadoop dfs -copyFromLocal ./words.txt /user/training/words.txt
-hadoop dfs -copyFromLocal ./data/access_log_0 /user/training/pig-apache/input/access_log_0
+hdfs dfs -mkdir /user/training
+hdfs dfs -copyFromLocal ./words.txt /user/training/words.txt
+hdfs dfs -mkdir -p /user/training/pig-apache/input/
+hdfs dfs -copyFromLocal ./data/access_log_0 /user/training/pig-apache/input/access_log_0
