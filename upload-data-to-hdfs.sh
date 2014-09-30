65979092eab09ea0c8c2842fe127c206b57ccdf8
Radek <radek@dataminelab.com>
2014-09-30 11:13:55 -0700

    Changed paths

--- upload-data-to-hdfs.sh
+++ upload-data-to-hdfs.sh
@@ -1,4 +1,3 @@
-hdfs dfs -mkdir /user/training
-hdfs dfs -copyFromLocal ./words.txt /user/training/words.txt
+hdfs dfs -copyFromLocal ./words.txt /user/root/words.txt
 hdfs dfs -mkdir -p /user/training/pig-apache/input/
-hdfs dfs -copyFromLocal ./data/access_log_0 /user/training/pig-apache/input/access_log_0
+hdfs dfs -copyFromLocal ./data/access_log_0 /user/root/pig-apache/input/access_log_0
