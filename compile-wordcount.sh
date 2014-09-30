140474a61025244e4b2f31dbcf79d7e35e54f87e
Radek <radek@dataminelab.com>
2014-09-30 06:33:20 -0700

    Fixed word count java example

--- compile-wordcount.sh
+++ compile-wordcount.sh
@@ -1,3 +1,3 @@
-export HADOOP_HOME=/usr/lib/hadoop-0.20
-javac -classpath ${HADOOP_HOME}/hadoop-*-core.jar -d java/target java/src/org/apache/WordCount.java
+javac -cp `hadoop classpath`:. -d java/target java/src/org/apache/WordCount.java
 jar -cvf ./wordcount.jar -C java/target .
+
--- streaming-mapreduce.sh
+++ streaming-mapreduce.sh
@@ -1,1 +1,1 @@
-hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -files ./mapper.py ./reducer.py -mapper ./mapper.py -reducer ./reducer.py -input /user/training/words.txt -output /user/training/words-output
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -files ./mapper.py,./reducer.py -mapper ./mapper.py -reducer ./reducer.py -input /user/training/words.txt -output /user/training/words-output
