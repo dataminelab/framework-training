f31a51f0419363955928f4e1a4c81e536f6abe56
Radek Maciaszek <radek@a4g.com>
2017-02-21 15:04:07 +0000

    Updates

--- upload-data-to-hdfs.sh
+++ upload-data-to-hdfs.sh
@@ -1,3 +1,3 @@
-hadoop dfs -copyFromLocal ./words.txt /user/root/words.txt
-hadoop dfs -mkdir -p /user/root/pig-apache/input/
-hadoop dfs -copyFromLocal ./data/access_log_0 /user/root/pig-apache/input/access_log_0
+hdfs dfs -copyFromLocal ./words.txt /user/root/words.txt
+hdfs dfs -mkdir -p /user/root/pig-apache/input/
+hdfs dfs -copyFromLocal ./data/access_log_0 /user/root/pig-apache/input/access_log_0
